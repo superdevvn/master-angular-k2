118b6f666b0df5646be0544e1575ed1dd8f228af
ledtin97 <ledtin97@gmail.com>
2018-09-16 11:52:35 +0700

    demo buổi 2

---
+++ students/Tin/02-ES6/src/enhanced-object-literals.ts
@@ -0,0 +1,10 @@
+// class Person1 {
+//     firstName: string;
+//     lastName: string;
+// }
+
+// function hello3(firstName: string,lastName: string ){
+//     var person = {firstName, lastName};
+//     console.log(person);
+// }
+// hello3('Super','Dev')
