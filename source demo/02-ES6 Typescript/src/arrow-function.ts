0fe7c1c6eacc8a6f6bfa1eea3573d165824f7dff
Trần Thuận Nghĩa <ghost.of.dark.peter@gmail.com>
2018-09-16 11:48:50 +0700

    - Demo 02

---
+++ source demo/02-ES6 Typescript/src/arrow-function.ts
@@ -0,0 +1,27 @@
+// var hello3 = (firstName: string, lastName: string) => {
+//     console.log(firstName, lastName);
+// }
+
+// hello3('Super', 'Dev');
+
+// class Person3 {
+//     hello: Function;
+//     call(hello: Function) {
+//         this.hello = hello;
+//         this.hello('Super', 'Dev');
+//     }
+// }
+
+// class Person4 {
+//     key = `I'm SuperMan`;
+//     hello = (firstName: string, lastName: string) => {
+//         console.log(this);
+//         console.log(this.key);
+//         console.log(firstName, lastName);
+//     }
+// }
+// var person3 = new Person3();
+// var person4 = new Person4();
+
+// person3.call(person4.hello);
+// person4.hello('Super','Dev');
---
+++ source demo/02-ES6 Typescript/src/enhanced-object-literals.ts
@@ -0,0 +1,10 @@
+// class Person1 {
+//     firstName: string;
+//     lastName: string;
+// }
+// function hello(firstName: string, lastName: string) {
+//     var person = { firstName, lastName };
+//     console.log(person);
+// }
+
+// hello('Super', 'Dev');
