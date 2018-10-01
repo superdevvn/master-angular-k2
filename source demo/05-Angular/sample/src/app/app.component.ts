dc8eab4f9b299b260b57cdd733d04b7d02aaac9c
Trần Thuận Nghĩa <ghost.of.dark.peter@gmail.com>
2018-10-01 20:29:32 +0700

    - 05- Angular

---
+++ source demo/05-Angular/sample/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'SuperDev';
+  constructor(){
+    console.log('constructor');
+  }
+  ngOnInit(){
+    console.log(document.getElementById(this.title));
+    console.log('ngOnInit');
+  }
+  ngAfterViewInit(){
+    console.log(document.getElementById(this.title));
+    console.log('ngAfterViewInit');
+  }
+}
