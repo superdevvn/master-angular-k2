b574cb78e8ca2459182b18b3482c2583e8ef43b7
Trần Thuận Nghĩa <ghost.of.dark.peter@gmail.com>
2018-12-08 10:20:33 +0700

    - Add demo 15 16

---
+++ source demo/16-Enviroment/sample-enviroment/src/environments/environment.model.ts
@@ -0,0 +1,5 @@
+export interface Enviroment {
+  production: boolean,
+  host: string,
+  logoUrl: string
+}
---
+++ source demo/16-Enviroment/sample-enviroment/src/environments/environment.testing.ts
@@ -0,0 +1,7 @@
+import { Enviroment } from "./environment.model";
+
+export const environment: Enviroment = {
+  production: true,
+  host: 'TESTING',
+  logoUrl: 'TEST'
+};
